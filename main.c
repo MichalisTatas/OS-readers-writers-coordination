73df024d5dbfafadca4b189751a78084e44e19a9
MichalisTatas <tatas.michalis@gmail.com>
2019-11-20 23:31:42 +0200

    added child processes

--- main.c
+++ main.c
@@ -4,20 +4,24 @@
 #include <sys/wait.h>
 
 /*
- *i must create a shared memory array with size input from command line
- *create readers writers do thei thing
- *when someone reads an index increase its readerCounter
- *same goes for writers
- *lastly when readers and writers finish their job
- *the coordinator iterates the whole array
- *and prints data like readerCounter, writerCounter
- *time spent for each action etc
+ * i must create a shared memory array with size input from command line
+ * create readers writers do thei thing
+ * when someone reads an index increase its readerCounter
+ * same goes for writers
+ * lastly when readers and writers finish their job
+ * the coordinator iterates the whole array
+ * and prints data like readerCounter, writerCounter
+ * time spent for each action etc
  */
 
+/*
+ * semaphore functions in one .h
+ * 
+ */
 int main(int argc, char* argv[])
 {
-
     pid_t pid;
+
     for (int i=0; i<atoi(argv[1]); i++) {    //creates childs bases on input
 
         if ( (pid = fork()) == -1) {
@@ -29,6 +33,8 @@ int main(int argc, char* argv[])
             break;
 
     }
+
     printf("hello world \n");
+    exit(0);
     return 0;
 }
